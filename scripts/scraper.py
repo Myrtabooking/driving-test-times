813a5bf6b32802bc65484fe7552d05c51d4eeb5d
Myrtabooking <allimehdi24@gmail.com>
2025-01-28 14:17:51 +1100

    Update scraper.py

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -14,18 +14,10 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--disable-infobars")
-chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-chrome_options.binary_location = "/usr/bin/google-chrome"  # Add this line
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
-# Add this line before creating the driver
-service = webdriver.ChromeService(executable_path="/usr/local/bin/chromedriver")
-
-# Modify the driver creation
-driver = webdriver.Chrome(service=service, options=chrome_options)
+# Just this simple driver initialization
+driver = webdriver.Chrome(options=chrome_options)
 
 
 try:
