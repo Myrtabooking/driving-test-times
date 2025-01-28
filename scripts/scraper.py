78dc969215499faaa1e993066e2d7f3ea4728931
Myrtabooking <allimehdi24@gmail.com>
2025-01-28 20:03:26 +1100

    Update scraper.py

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -2,14 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 import time
 import json
-import os  # Import os to access environment variables
+import os
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")  # Uncomment this line to run in headless mode
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920,1080")
@@ -21,14 +20,17 @@ driver = webdriver.Chrome(options=chrome_options)
 
 try:
     driver.get("https://www.myrta.com/wps/portal/extvp/myrta/login/")
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 20)  # Increased wait time
+
+    # Debugging: Print the page source to see if the page loaded correctly
+    print(driver.page_source)
 
     # Use environment variables for license number and password
     license_number = wait.until(EC.visibility_of_element_located((By.ID, "widget_cardNumber")))
-    license_number.send_keys(os.environ['LICENSE_NUMBER'])  # Use environment variable
+    license_number.send_keys(os.environ['LICENSE_NUMBER'])
 
     password = wait.until(EC.visibility_of_element_located((By.ID, "widget_password")))
-    password.send_keys(os.environ['PASSWORD'])  # Use environment variable
+    password.send_keys(os.environ['PASSWORD'])
 
     login_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@id='nextButton_label']")))
     login_button.click()
